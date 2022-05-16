460a1495e6c7e2dd05b0af37bbb371646aa61477
999luo <zhuzhu95327>
2022-05-16 18:36:07 +0800

    修改

--- pages/evaluation/overview/overview.js
+++ pages/evaluation/overview/overview.js
@@ -78,7 +78,8 @@ Page({
     wx: if (testtype == 2) {
       wx.navigateTo({
         url: '/pages/evaluation/dynamicDetail/dynamicDetail',
-        // url: '/pages/evaluation/demo/demo',
+   
+             // url: '/pages/evaluation/demo/demo',
       })
     }
 
