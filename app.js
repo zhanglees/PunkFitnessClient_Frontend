6f9ced6d5896bc8ba10c58e54f03f84529e8de62
zuoli <zuolio.o@gmail.com>
2022-05-30 18:36:01 +0800

    0530

--- app.js
+++ app.js
@@ -2,6 +2,26 @@
 const req = require('/utils/req.js');
 App({
     onLaunch() {
+        this.overShare()
+    },
+    /**
+     * 开启朋友圈分享功能
+     * @description 监听路由切换/自动执行
+     * @return void
+     */
+    overShare() {
+        wx.onAppRoute((res) => {
+            // console.log('route', res)
+            let pages = getCurrentPages()
+            let view = pages[pages.length - 1]
+            if (view) {
+                wx.showShareMenu({
+                    menus: ['shareAppMessage', 'shareTimeline'],
+                    success(res) {},
+                    fail(e) {}
+                })
+            }
+        })
     },
     req: req,
     globalData: {
