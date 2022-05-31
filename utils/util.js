d6bd8690515162c5577c87ab837baade5e3ac476
zuoli <zuolio.o@gmail.com>
2022-05-31 21:08:44 +0800

    0531

--- utils/util.js
+++ utils/util.js
@@ -1,29 +1,34 @@
 const formatTime = date => {
-  const year = date.getFullYear()
-  const month = date.getMonth() + 1
-  const day = date.getDate()
-  const hour = date.getHours()
-  const minute = date.getMinutes()
-  const second = date.getSeconds()
-
-  return `${[year, month, day].map(formatNumber).join('/')} ${[hour, minute, second].map(formatNumber).join(':')}`
-}
-
-const formatNumber = n => {
-  n = n.toString()
-  return n[1] ? n : `0${n}`
-}
-const formatDate = date => {
-  if(date){
     const year = date.getFullYear()
     const month = date.getMonth() + 1
     const day = date.getDate()
-    return `${[year, month, day].map(formatNumber).join('-')}`
-  }else{
-    return '';
-  }
+    const hour = date.getHours()
+    const minute = date.getMinutes()
+    const second = date.getSeconds()
+
+    return `${[year, month, day].map(formatNumber).join('/')} ${[hour, minute, second].map(formatNumber).join(':')}`
+}
+
+const formatNumber = n => {
+    n = n.toString()
+    return n[1] ? n : `0${n}`
+}
+
+const formatDateStr = date => {
+    return date ? date.match(/[0-9]+-[0-9]+-[0-9]+/)[0].replace(/-/g, '/') : '--';
+}
+const formatDate = date => {
+    if (date) {
+        const year = date.getFullYear()
+        const month = date.getMonth() + 1
+        const day = date.getDate()
+        return `${[year, month, day].map(formatNumber).join('/')}`
+    } else {
+        return '';
+    }
 }
 module.exports = {
-  formatTime,
-  formatDate
+    formatTime,
+    formatDate,
+    formatDateStr
 }
